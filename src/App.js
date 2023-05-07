5b0133b19c28142a6233db50ffaa507f9332113b
Anjitha Mohanan <anjithamohanan263@gmail.com>
2023-05-07 22:04:14 +0530

    otptask

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Verification_Otp from './components/Verification_Otp';
+
+function App() {
+  return (
+   <Verification_Otp/>
+  );
+}
+
+export default App;
